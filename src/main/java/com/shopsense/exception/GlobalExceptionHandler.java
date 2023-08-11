a43e074b9fa6e040e400d1f208997e7f77d0968b
Hassan Mahfuj <itsmahfuj@gmail.com>
2023-08-12 03:39:01 +0600

    image upload added

---
+++ src/main/java/com/shopsense/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.shopsense.exception;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<HashMap<String, String>> handleAllException(Exception e) {
+		HashMap<String, String> m = new HashMap<>();
+		m.put("status", e.toString());
+		return new ResponseEntity<HashMap<String, String>>(m, HttpStatus.OK);
+	}
+}

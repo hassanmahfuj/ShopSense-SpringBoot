0c97d9f17958764ce1a9c086864232051161e57f
Hassan Mahfuj <itsmahfuj@gmail.com>
2023-08-30 03:40:00 +0600

    jasper report and dashboard bg color

--- src/main/java/com/shopsense/security/SecurityConfig.java
+++ src/main/java/com/shopsense/security/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
 								"/collectionpoint/search",
 								"/upload",
 								"/uploads/**",
+								"/reports/**",
 								"/**"
 						).permitAll()
 						.anyRequest().authenticated())
